ea36735903c0c10985376bf1f5679152c377ed47
Bethwel <kiptoobethwel@01>
2023-11-28 16:10:53 +0300

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #/usr/bin/env bash
 #settin a puppet command for short cut
 
-file {'etc/ssh/ssh_config' :
+file{'etc/ssh/ssh_config':
 	ensure = present,
 
-content =>"
-	#ssh client configuration
-	host*
-	PasswordAuthentication no
-	IdentifyFile ~/.ssh/school"
+	content =>
+		#ssh client configuration
+		host*
+		PasswordAuthentication no
+		IdentifyFile ~/.ssh/school
 }
