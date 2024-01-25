fa78e2c397c6fdfd6c1a38aea8670a3ec4234274
Dev-Alany <devalanyheman2@gmail.com>
2024-01-24 23:23:59 -0800

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/
+# Fix error 500 server error of a LAMP stack
+exec { 'sed_replace':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin:/usr/bin',
+  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
+}
